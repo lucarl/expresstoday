0f8bfb58fb86e18aca0a17759e657913f388fa3a
lucarl <carl.e.w.lundborg@gmail.com>
2019-05-09 13:09:27 +0200

    test

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 5000;
 const api_helper = require('./API_helper')
+const path = require('path');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -53,10 +54,10 @@ app.get('/getAPIVeg', (req, res) => {
 
 if (process.env.NODE_ENV === 'production') {
     // Serve any static files
-    app.use(express.static(path.join(__dirname, 'client')));
+    app.use(express.static(path.join(__dirname, 'client/build')));
     // Handle React routing, return all requests to React app
     app.get('*', function(req, res) {
-      res.sendFile(path.join(__dirname, 'client', 'index.html'));
+      res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
     });
   }
 
